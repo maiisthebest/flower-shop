ecf361709b13ef49d7a2626f47d0c0c9a5fb1ddd
Phuong Mai Nguyen <phuong.nguyen@aotech.net.au>
2023-10-23 23:01:37 +1100

    Refactor to derive the value of filteredFlowers from flowers and filters

--- client/src/components/Flowers/Flowers.js
+++ client/src/components/Flowers/Flowers.js
@@ -1,12 +1,11 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Cards from "../Cards/Cards";
 import Filter from "../Filter/Filter";
 import "./Flowers.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
 const Flowers = () => {
   const [flowers, setFlowers] = useState([]);
-  const [filteredFlowers, setFilteredFlowers] = useState([]);
   const [filters, setFilters] = useState({
     colour: "any",
     favoured: "any",
@@ -15,37 +14,36 @@ const Flowers = () => {
   const fetchFlowers = async () => {
     const response = await axios.get("http://localhost:4000/flowers");
     setFlowers(response.data);
-    setFilteredFlowers(response.data);
   };
 
   useEffect(() => {
     fetchFlowers();
   }, []);
 
-  useEffect(() => {
-    let filteredFlowersTemp = [...flowers];
+  function getFilteredFlowers() {
+    let filteredFlowers = [...flowers];
 
     if (filters.favoured !== "any") {
-      filteredFlowersTemp = filteredFlowersTemp.filter(
+      filteredFlowers = filteredFlowers.filter(
         (flower) =>
           flower.favoured === (filters.favoured === "favoured" ? true : false)
       );
     }
 
     if (filters.colour !== "any") {
-      filteredFlowersTemp = filteredFlowersTemp.filter(
+      filteredFlowers = filteredFlowers.filter(
         (flower) => flower.colour === filters.colour
       );
     }
 
-    setFilteredFlowers(filteredFlowersTemp);
-  }, [filters]);
+    return filteredFlowers;
+  }
 
   return (
     <div className="container">
       <div className="app-container">
         <Filter filters={filters} setFilters={setFilters} />
-        <Cards flowers={filteredFlowers} setFlowers={setFlowers} />
+        <Cards flowers={getFilteredFlowers()} setFlowers={setFlowers} />
       </div>
     </div>
   );
