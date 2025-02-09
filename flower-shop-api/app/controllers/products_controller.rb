67d721597b135282bb6fa89fd3160ea0c8282a61
Phuong Mai Nguyen <phuong.nguyen@aotech.net.au>
2025-02-09 22:05:49 +1100

    feat: Initial commit - setup Rails API

---
+++ flower-shop-api/app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+  def show
+    product = {
+      name: "Pink Lily",
+      price: 10,
+      image: "https://images.unsplash.com/photo-1561897519-6e4fbd1fbc41?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+
+    render json: product
+  end
+end
