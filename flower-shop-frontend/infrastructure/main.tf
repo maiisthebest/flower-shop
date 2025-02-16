44792713b2c2f63d55e48f6ead55e1e263ce276d
Phuong Mai Nguyen <phuong.nguyen@aotech.net.au>
2025-02-16 20:16:58 +1100

    feat: [Frontend] Setup infrastructure

---
+++ flower-shop-frontend/infrastructure/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "flower-shop-frontend-state-bucket"
+    key            = "flower-shop-frontend/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "flower-shop-frontend"
+
+  tags = {
+    Name = "Flower Shop Frontend"
+  }
+  
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_config" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_public_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::flower-shop-frontend/*"
+    }
+  ]
+}
+EOF
+}
