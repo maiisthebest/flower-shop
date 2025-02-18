f451a4efc8d7cf8b2650f3cd6f1d0c3076da0155
Phuong Mai Nguyen <phuong.nguyen@aotech.net.au>
2025-02-19 08:45:26 +1100

    feat: [backend] Refactor products show to read from json file

--- flower-shop-api/spec/requests/products_spec.rb
+++ flower-shop-api/spec/requests/products_spec.rb
@@ -2,11 +2,26 @@ require "rails_helper"
 
 RSpec.describe "Products", type: :request do
   describe "GET /products/show" do
-    it "returns a list of product details" do
+    let(:mock_flowers) do
+      [
+        { "name" => "Mock Rose", "imageUrl" => "https://abc.com/mock-rose.jpg", "price" => 9.99 },
+        { "name" => "Mock Tulip", "imageUrl" => "https://abc.com/mock-tulip.jpg", "price" => 7.99 },
+      ]
+    end
+
+    before do
+      allow(File).to receive(:read).with(Rails.root.join("config", "flowers.json")).and_return(mock_flowers.to_json)
+    end
+
+    it "returns a list of products" do
       get "/products/show"
 
       expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body).first).to include("name", "price", "imageUrl")
+
+      products_response = JSON.parse(response.body)
+      expect(products_response.size).to eq(mock_flowers.size)
+      expect(products_response.first).to eq(mock_flowers.first)
+      expect(products_response.last).to eq(mock_flowers.last)
     end
   end
 end
